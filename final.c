da79de123384de13bdd0923af4c3d83075b66293
Justine Xue <jxue5194@uni.sydney.edu.au>
2025-04-04 16:39:04 +1100

    Updating gitignore

--- final.c
+++ final.c
@@ -52,11 +52,6 @@ typedef struct {
     double **M_3;
 } ThreadDataDouble;
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <string.h>
-
 void compare_files(const char *filename_1, const char *filename_2) {
     FILE *file_1 = fopen(filename_1, "r");
     if (file_1 == NULL) {
@@ -142,8 +137,6 @@ void multiply_matrix_float_seq(float **M_1, float **M_2, float **M_3, int rows, 
 
 void* multiply_block_float_par(void* arg) {
     ThreadDataFloat* data = (ThreadDataFloat*)arg;
-    // printf("Thread %d - Multiply block for start_row: %d end_row: %d\nstart_col: %d end_col: %d\n", data->thread_id, data->start_row, data->end_row, data->start_col, data->end_col);
-    // printf("Common dim limit is %d, common dim is %d\n", data->common_dim_limit, data->common_dim);
     for (int i = data->start_row; i < data->end_row; i++) {
         for (int j = data->start_col; j < data->end_col; j+=4) {
             for (int k = 0; k < data->common_dim_limit; k+=4){
@@ -166,8 +159,6 @@ void* multiply_block_float_par(void* arg) {
                 data->M_3[i][j+3] += data->M_1[i][k+1] * data->M_2[k+1][j+3];
                 data->M_3[i][j+3] += data->M_1[i][k+2] * data->M_2[k+2][j+3];
                 data->M_3[i][j+3] += data->M_1[i][k+3] * data->M_2[k+3][j+3];
-                // data->M_3[i][j] += data->M_1[i][k] * data->M_2[k][j];
-                // printf("M_3[%d][%d] += %f * %f]\n", i, j, data->M_1[i][k], data->M_2[k][j]);
             }
             for (int k = data->common_dim_limit; k < data->common_dim; k++){
                 data->M_3[i][j] += data->M_1[i][k] * data->M_2[k][j];
@@ -182,8 +173,6 @@ void* multiply_block_float_par(void* arg) {
 
 void* multiply_block_double_par(void* arg) {
     ThreadDataDouble* data = (ThreadDataDouble*)arg;
-    // printf("Thread %d - Multiply block for start_row: %d end_row: %d\nstart_col: %d end_col: %d\n", data->thread_id, data->start_row, data->end_row, data->start_col, data->end_col);
-    // printf("Common dim limit is %d, common dim is %d\n", data->common_dim_limit, data->common_dim);
     for (int i = data->start_row; i < data->end_row; i++) {
         for (int j = data->start_col; j < data->end_col; j+=4) {
             for (int k = 0; k < data->common_dim_limit; k+=4){
@@ -206,8 +195,6 @@ void* multiply_block_double_par(void* arg) {
                 data->M_3[i][j+3] += data->M_1[i][k+1] * data->M_2[k+1][j+3];
                 data->M_3[i][j+3] += data->M_1[i][k+2] * data->M_2[k+2][j+3];
                 data->M_3[i][j+3] += data->M_1[i][k+3] * data->M_2[k+3][j+3];
-                // data->M_3[i][j] += data->M_1[i][k] * data->M_2[k][j];
-                // printf("M_3[%d][%d] += %f * %f]\n", i, j, data->M_1[i][k], data->M_2[k][j]);
             }
             for (int k = data->common_dim_limit; k < data->common_dim; k++){
                 data->M_3[i][j] += data->M_1[i][k] * data->M_2[k][j];
@@ -260,13 +247,10 @@ void multiply_matrix_double_par(double **M_1, double **M_2, double **M_3, int ro
     int block_cols = floor(cols / block_size);
     int thread_index = 0;
     int common_dim_limit = floor(common_dim/4) * 4;
-    // printf("block rows is %d, block cols is %d\n", block_rows, block_cols);
 
-    // printf("Common dim is %d\n", common_dim);
 
     for (int i = 0; i < block_rows; i++) {
         for (int j = 0; j < block_cols; j++) {
-            // printf("Iterating for block[%d][%d]\n", i, j);
             if (thread_index == num_threads){
                 thread_index = 0;
             }
@@ -281,7 +265,6 @@ void multiply_matrix_double_par(double **M_1, double **M_2, double **M_3, int ro
             thread_data[thread_index].M_2 = M_2;
             thread_data[thread_index].M_3 = M_3;
             pthread_create(&threads[thread_index], NULL, multiply_block_double_par, &thread_data[thread_index]);
-            // pthread_join(threads[thread_index], NULL);
             thread_index++;
         }
     }
@@ -291,8 +274,6 @@ void multiply_matrix_double_par(double **M_1, double **M_2, double **M_3, int ro
     }
 
     // Process the leftover rows and columns
-    // printf("Cleaning up\n");
-    // printf("Multiply block for start_row: %d end_row: %d\nstart_col: %d end_col: %d\n", block_rows * block_size, rows-1, 0, cols-1);
     for (int i = block_rows * block_size; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             for (int k = 0; k < common_dim; k++){
@@ -300,7 +281,6 @@ void multiply_matrix_double_par(double **M_1, double **M_2, double **M_3, int ro
             }
         }
     }
-    // printf("Multiply block for start_row: %d end_row: %d\nstart_col: %d end_col: %d\n", 0, rows-1, block_cols * block_size, cols-1);
     for (int j = block_cols * block_size; j < cols; j++) {
         for (int i = 0; i < block_rows * block_size; i++) {
             for (int k = 0; k < common_dim; k++){
@@ -314,19 +294,14 @@ void multiply_matrix_float_par(float **M_1, float **M_2, float **M_3, int rows, 
     pthread_t threads[num_threads];
     ThreadDataFloat thread_data[num_threads];
 
-    // printf("Common dim is %d\n", common_dim);
-
     // Partition the matrix into blocks and assign to worker threads
     int block_rows = floor(rows / block_size);
     int block_cols = floor(cols / block_size);
     int thread_index = 0;
     int common_dim_limit = floor(common_dim/4) * 4;
-    // printf("block rows is %d, block cols is %d\n", block_rows, block_cols);
-
 
     for (int i = 0; i < block_rows; i++) {
         for (int j = 0; j < block_cols; j++) {
-            // printf("Iterating for block[%d][%d]\n", i, j);
             if (thread_index == num_threads){
                 thread_index = 0;
             }
@@ -341,7 +316,6 @@ void multiply_matrix_float_par(float **M_1, float **M_2, float **M_3, int rows, 
             thread_data[thread_index].M_2 = M_2;
             thread_data[thread_index].M_3 = M_3;
             pthread_create(&threads[thread_index], NULL, multiply_block_float_par, &thread_data[thread_index]);
-            // pthread_join(threads[thread_index], NULL);
             thread_index++;
         }
     }
@@ -352,8 +326,6 @@ void multiply_matrix_float_par(float **M_1, float **M_2, float **M_3, int rows, 
     }
 
     // Process the leftover rows and columns
-    // printf("Cleaning up\n");
-    // printf("Multiply block for start_row: %d end_row: %d\nstart_col: %d end_col: %d\n", block_rows * block_size, rows-1, 0, cols-1);
     for (int i = block_rows * block_size; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             for (int k = 0; k < common_dim; k++){
@@ -361,7 +333,6 @@ void multiply_matrix_float_par(float **M_1, float **M_2, float **M_3, int rows, 
             }
         }
     }
-    // printf("Multiply block for start_row: %d end_row: %d\nstart_col: %d end_col: %d\n", 0, rows-1, block_cols * block_size, cols-1);
     for (int j = block_cols * block_size; j < cols; j++) {
         for (int i = 0; i < block_rows * block_size; i++) {
             for (int k = 0; k < common_dim; k++){
@@ -488,11 +459,6 @@ int main(int argc, char *argv[]){
         }
     }
 
-    // printf("m is %d\n", m);
-    // printf("k is %d\n", k);
-    // printf("l is %d\n", l);
-    // printf("n is %d\n", n);
-
     int t_rows, t_cols;
     int t_type;
     //determining optimal T by comparing the number of scalar multiplications which will occur
@@ -503,24 +469,17 @@ int main(int argc, char *argv[]){
         t_rows = m;
         t_cols = l;
         t_type = A_TIMES_B;
-        // printf("A TIMES B\n");
     } else {
         // otherwise, A * (B * C) is more efficient
         t_rows = k;
         t_cols = n;
         t_type = B_TIMES_C;
-        // printf("B times C\n");
     }
     srand(SID);
 
-    // int blockSize = L1_CACHE;
     int blockSize = floor(sqrt(L1_CACHE/3));
-    // printf("blockSize: %d\n", blockSize);
-    // optimal block size is sqrt(cache size/3)
 
     if (array_type == FLOAT){
-        // printf("type is float\n");
-        // printf("num_threads is %d\n", num_threads);
 
         float **A = setup_matrix_float(A, m, k, array_type, RANDOM, "A");
         float **B = setup_matrix_float(B, k, l, array_type, RANDOM, "B");
@@ -533,18 +492,10 @@ int main(int argc, char *argv[]){
         struct timeval start_seq, end_seq;
         gettimeofday(&start_seq, NULL);
         if (t_type == A_TIMES_B){
-            // printf("Multiplying matrix A * B = T, where A = (%d x %d), B = (%d x %d), T = (%d x %d)\n",
-            // m, k, k, l, t_rows, t_cols);
             multiply_matrix_float_seq(A, B, T_seq, m, k, l);
-            // printf("Multiplying matrix T * C = D, where T = (%d x %d), C = (%d x %d), D = (%d x %d)\n",
-                // t_rows, t_cols, l, n, m, n);
             multiply_matrix_float_seq(T_seq, C, D_seq, t_rows, t_cols, n);
         } else if (t_type == B_TIMES_C){
-            // printf("Multiplying matrix B * C = T, where B = (%d x %d), C = (%d x %d), T = (%d x %d)\n",
-                // k, l, l, n, t_rows, t_cols);
             multiply_matrix_float_seq(B, C, T_seq, k, l, n);
-            // printf("Multiplying matrix A * T = D, where A = (%d x %d), T = (%d x %d), D = (%d x %d)\n",
-                // m, k, t_rows, t_cols, m, n);
             multiply_matrix_float_seq(A, T_seq, D_seq, m, t_rows, t_cols);
         }
         gettimeofday(&end_seq, NULL);
@@ -559,18 +510,10 @@ int main(int argc, char *argv[]){
         struct timeval start_par, end_par;
         gettimeofday(&start_par, NULL);
         if (t_type == A_TIMES_B){
-            // printf("Multiplying matrix A * B = T, where A = (%d x %d), B = (%d x %d), T = (%d x %d)\n",
-            // m, k, k, l, t_rows, t_cols);
             multiply_matrix_float_par(A, B, T_par, m, k, l, num_threads, blockSize);
-            // printf("Multiplying matrix T * C = D, where T = (%d x %d), C = (%d x %d), D = (%d x %d)\n",
-            //     t_rows, t_cols, l, n, m, n);
             multiply_matrix_float_par(T_par, C, D_par, t_rows, t_cols, n, num_threads, blockSize);
         } else if (t_type == B_TIMES_C){
-            // printf("Multiplying matrix B * C = T, where B = (%d x %d), C = (%d x %d), T = (%d x %d)\n",
-            //     k, l, l, n, t_rows, t_cols);
             multiply_matrix_float_par(B, C, T_par, k, l, n, num_threads, blockSize);
-            // printf("Multiplying matrix A * T = D, where A = (%d x %d), T = (%d x %d), D = (%d x %d)\n",
-            //     m, k, t_rows, t_cols, m, n);
             multiply_matrix_float_par(A, T_par, D_par, m, t_rows, t_cols, num_threads, blockSize);
         }
         gettimeofday(&end_par, NULL);
@@ -612,8 +555,6 @@ int main(int argc, char *argv[]){
         free(T_par);
 
     } else if (array_type == DOUBLE){
-        printf("type is double\n");
-        printf("num_threads is %d\n", num_threads);
         double **A = setup_matrix_double(A, m, k, array_type, RANDOM, "A");
         double **B = setup_matrix_double(B, k, l, array_type, RANDOM, "B");
         double **C = setup_matrix_double(C, l, n, array_type, RANDOM, "C");
@@ -625,18 +566,10 @@ int main(int argc, char *argv[]){
         struct timeval start_seq, end_seq;
         gettimeofday(&start_seq, NULL);
         if (t_type == A_TIMES_B){
-            // printf("Multiplying matrix A * B = T, where A = (%d x %d), B = (%d x %d), T = (%d x %d)\n",
-            // m, k, k, l, t_rows, t_cols);
             multiply_matrix_double_seq(A, B, T_seq, m, k, l);
-            // printf("Multiplying matrix T * C = D, where T = (%d x %d), C = (%d x %d), D = (%d x %d)\n",
-                // t_rows, t_cols, l, n, m, n);
             multiply_matrix_double_seq(T_seq, C, D_seq, t_rows, t_cols, n);
         } else if (t_type == B_TIMES_C){
-            // printf("Multiplying matrix B * C = T, where B = (%d x %d), C = (%d x %d), T = (%d x %d)\n",
-                // k, l, l, n, t_rows, t_cols);
             multiply_matrix_double_seq(B, C, T_seq, k, l, n);
-            // printf("Multiplying matrix A * T = D, where A = (%d x %d), T = (%d x %d), D = (%d x %d)\n",
-                // m, k, t_rows, t_cols, m, n);
             multiply_matrix_double_seq(A, T_seq, D_seq, m, t_rows, t_cols);
         }
         gettimeofday(&end_seq, NULL);
@@ -651,18 +584,10 @@ int main(int argc, char *argv[]){
         struct timeval start_par, end_par;
         gettimeofday(&start_par, NULL);
         if (t_type == A_TIMES_B){
-            // printf("Multiplying matrix A * B = T, where A = (%d x %d), B = (%d x %d), T = (%d x %d)\n",
-            // m, k, k, l, t_rows, t_cols);
             multiply_matrix_double_par(A, B, T_par, m, k, l, num_threads, blockSize);
-            // printf("Multiplying matrix T * C = D, where T = (%d x %d), C = (%d x %d), D = (%d x %d)\n",
-            //     t_rows, t_cols, l, n, m, n);
             multiply_matrix_double_par(T_par, C, D_par, t_rows, t_cols, n, num_threads, blockSize);
         } else if (t_type == B_TIMES_C){
-            // printf("Multiplying matrix B * C = T, where B = (%d x %d), C = (%d x %d), T = (%d x %d)\n",
-            //     k, l, l, n, t_rows, t_cols);
             multiply_matrix_double_par(B, C, T_par, k, l, n, num_threads, blockSize);
-            // printf("Multiplying matrix A * T = D, where A = (%d x %d), T = (%d x %d), D = (%d x %d)\n",
-            //     m, k, t_rows, t_cols, m, n);
             multiply_matrix_double_par(A, T_par, D_par, m, t_rows, t_cols, num_threads, blockSize);
         }
         gettimeofday(&end_par, NULL);
